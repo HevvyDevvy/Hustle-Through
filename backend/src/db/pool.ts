275428a2a2731937e94fcb4aa7e83fe0fd06c543
Blu Corbel <blucorbel35@gmail.com>
2026-08-30 15:26:27 +0100

    Update pool.ts

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -7,6 +7,6 @@ export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-pool.on("error", (err) => {
+pool.on("error", (err: Error) => {
   console.error("Unexpected Postgres error", err);
 });
